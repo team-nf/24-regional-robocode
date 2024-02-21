c3510d79ec1a4e673e49ce8f743eed269dfcfac6
Tuna Gül <tuna.gul@nfrproducts.com>
2024-02-21 04:55:44 +0300

    Implement Intake Subsystem
    Co-authored-by: Yüşa <yehpop@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,46 +1,70 @@
 package frc.robot.subsystems;
 
-import java.util.function.DoubleSupplier;
-
-// import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.IntakeConstants;
+import edu.wpi.first.wpilibj.Encoder;
 
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
-  private final WPI_VictorSPX intakeMotor;
+  private final WPI_VictorSPX intakeMotor = new WPI_VictorSPX(IntakeConstants.kIntakeMotorId);;
+  private final PIDController intakeController = new PIDController(
+    IntakeConstants.kIntakeMotorKp,
+    IntakeConstants.kIntakeMotorKi,
+    IntakeConstants.kIntakeMotorKd
+  );
+  private final SimpleMotorFeedforward intakeFeedForward = new SimpleMotorFeedforward(
+    IntakeConstants.kIntakeMotorKs,
+    IntakeConstants.kIntakeMotorKv
+  );
+  private final Encoder intakeEncoder = new Encoder(
+    IntakeConstants.kIntakeEncoderA,
+    IntakeConstants.kIntakeEncoderB
+  );
+  private double currentTargetVel;
+
 
   public IntakeSubsystem() {
-    intakeMotor = new WPI_VictorSPX(IntakeConstants.kIntakeMotorId);
-
-    // Motor configuration
-    intakeMotor.setNeutralMode(NeutralMode.Brake);
-    intakeMotor.setInverted(true);
+    intakeMotor.configVoltageCompSaturation(Constants.nominalVoltage);
+    intakeMotor.enableVoltageCompensation(true);
+    currentTargetVel = 0;
   }
 
-  public void runIntake(double voltage) {
-    intakeMotor.setVoltage(voltage);
+  private void setVelOnce() {
+    double currentVel = intakeEncoder.getRate();
+    double pidValue = intakeController.calculate(currentVel, currentTargetVel);
+    double feedforwardValue = intakeFeedForward.calculate(currentTargetVel);
+
+    intakeMotor.set(pidValue + feedforwardValue);
   }
 
-  public void runIntake() {
-    runIntake(IntakeConstants.kMinVoltage);
-  }
-
-  public Command runIntakeCommand(DoubleSupplier voltageSupplier) {
-    return run(() -> runIntake(voltageSupplier.getAsDouble()));
+  private void setVelOnce(double targetVel) {
+    currentTargetVel = targetVel;
+    setVelOnce();
   }
 
   public Command runIntakeCommand() {
-    return run(() -> runIntake());
+    return runOnce(() -> {
+        setVelOnce(IntakeConstants.kIntakeTargetVel);
+    });
   }
 
   public Command stopIntakeCommand() {
-    return run(() -> runIntake(0));
+    return runOnce(() -> {
+        setVelOnce(0);
+    });
+  }
+
+  /**
+   * WPILib Pid loopu sürekli dönmeli
+   */
+  @Override
+  public void periodic() {
+    setVelOnce();
   }
 }
