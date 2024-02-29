ccb15903a1087fd084a49d64cc8aa8cfc92ab448
Tuna Gül <tuna.gul@nfrproducts.com>
2024-02-29 16:31:15 +0300

    Update GetObjectCommand

--- src/main/java/frc/robot/commands/GetObjectCommand.java
+++ src/main/java/frc/robot/commands/GetObjectCommand.java
@@ -9,6 +9,8 @@ import frc.robot.subsystems.ShooterSubsystem;
 public class GetObjectCommand extends Command {
     private final ShooterSubsystem m_shooter;
     private final IntakeSubsystem m_intake;
+    private Command m_runMotorsCommand, m_stopMotorsCommand;
+    private boolean m_shooterGotObject = false;
 
     public GetObjectCommand(double angle, ShooterSubsystem shooterSubsystem, IntakeSubsystem intakeSubsystem) {
         m_intake = intakeSubsystem;
@@ -16,32 +18,51 @@ public class GetObjectCommand extends Command {
 
         addRequirements(m_intake);
         addRequirements(m_shooter);
+
+        m_shooterGotObject = m_shooter.hasObject();
     }
 
     @Override
     public void initialize() {
         /* Eğer shooterda obje varsa bu fonksiyonlar çalışmamalı*/
-        if (m_shooter.hasObject()) { return; }
+        if (m_shooterGotObject) { return; }
 
-        new SequentialCommandGroup(
+        m_runMotorsCommand = new SequentialCommandGroup(
             m_intake.runIntakeCommand(),
             m_shooter.runFeederCommand()
-        ).execute();
+        );
+
+        m_stopMotorsCommand = new SequentialCommandGroup(
+            m_intake.stopIntakeCommand(),
+            m_shooter.stopFeederCommand()
+        );
     }
 
     @Override
     public void execute() {
-        /** Obje gelene kadar bekle */
-        while (!m_shooter.hasObject()) {}
-        /* Gelince motorları durdur */
-        new SequentialCommandGroup(
-            m_intake.stopIntakeCommand(),
-            m_shooter.stopFeederCommand()
-        ).execute();
+        /* Öncelikle motorları çalıştır ve hızlanmasını bekle*/
+        if (!m_runMotorsCommand.isFinished() && m_shooterGotObject == false) {
+            m_runMotorsCommand.execute();
+            return;
+        }
+
+        /*
+         * Önceden shooterda obje görüldüyse hasObject
+         * fonksiyonunu baştan çağırmamak için 2 if var,
+         * çünkü sensör okumada delay olabilir
+        */
+        if (m_shooterGotObject == false) {
+            if (m_shooter.hasObject())
+                m_shooterGotObject = true;
+        }
+
+        if (m_shooterGotObject) {
+            m_stopMotorsCommand.execute();
+        }
     }
 
     @Override
     public boolean isFinished() {
-        return m_shooter.hasObject();
+        return m_shooterGotObject && m_stopMotorsCommand.isFinished();
     }
 }
